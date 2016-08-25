85f6f9eeff58574364ea433bc1d9c6f238290b3a
rouxbuciu <roux@acmelabs.ca>
2016-08-24 22:49:20 -0400

    Data entry for parks.

--- favourite-places/Darlington.swift
+++ favourite-places/Darlington.swift
@@ -11,7 +11,7 @@ import Foundation
 let darlington = Park(name: "darlington",
                       about: "\u{2022} Family camping experience close to home\n\u{2022} Great day-use and picnic facilities\n\u{2022} Group picnic and camping opportunities\n\u{2022} Nature trails\n\u{2022} A long sandy beach on Lake Ontario\n\u{2022} Interpretive programs and special events",
                       general: "\u{2022} Phone: 905-436-2036\n\u{2022} Size: 208.82 ha\n\u{2022} Year established: 1959\n\u{2022} Park Classification: Recreational \n\u{2022} Address:\n\t1600 Darlington Park Rd.\n\tBowmanville\n\tL1C 3K3",
-                      camping: [0: (heading: "", detail: "")],
+                      camping: [0: (heading: "\(cCar)", detail: "Darlington offers car camping in three different campgrounds; Lakeside, Cliffside and Hilltop. All of the sites in Lakeside Campground offer electrical service as do a number of sites in Cliffside and Hilltop Campgrounds. There are sites to accommodate all types of camping equipment from tents to large RVs and trailers. Barrier-free camping is also available.")],
                       activities: [0: (heading: "", detail: "")],
                       facilities: [0: (heading: "", detail: "")])
 
